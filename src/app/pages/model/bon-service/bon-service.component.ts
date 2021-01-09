29a0d879f66b7735ef4849ce749b4f422b94b7aa
mounalahrach <32428863+mounalahrach@users.noreply.github.com>
2021-01-08 18:00:45 -0800

    bonservice update

--- src/app/pages/model/bon-service/bon-service.component.ts
+++ src/app/pages/model/bon-service/bon-service.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { Client } from 'app/pages/entities/client';
 import { BonService } from '../../entities/bonService';
 import { BonServiceService } from './bon-service.service';
+import { Router } from '@angular/router';
+import { debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs/operators';
+import { Client } from 'app/pages/entities/client';
+import { ClientMorale } from 'app/pages/entities/clientMorale';
+import { clientPhysique } from 'app/pages/entities/clientPhysique';
+import { ClientMoraleService } from '../client-morale/client-morale.service';
+import { ClientPhysiqueService } from '../client-physique/client-physique.service';
+import { ClientService } from '../client/client.service';
+import { Observable, Subject } from 'rxjs';
+
 @Component({
   selector: 'app-bon-service',
   templateUrl: './bon-service.component.html',
@@ -10,13 +19,74 @@ import { BonServiceService } from './bon-service.service';
 export class BonServiceComponent implements OnInit {
 
   bonservice: BonService = new BonService();
+  clientphysique: clientPhysique = new clientPhysique();
+  clientPhysiques : clientPhysique [];
+  clientMorale: ClientMorale = new ClientMorale();
+  clientmorales: ClientMorale[];
+  client: Client = new Client();
+  datasource: Client[];
+
   dataSource: BonService[];
+  data = null  ;
+
+
+  data8 = [
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      email: 'Julianne.OConner@kory.org',
+      passed: 'Yes',
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      email: 'Lucio_Hettinger@annie.ca',
+      passed: 'No',
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      email: 'Karley_Dach@jasper.info',
+      passed: 'Yes',
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      email: 'Telly.Hoeger@billy.biz',
+      passed: 'No',
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      email: 'Sherwood@rosamond.me',
+      passed: 'Yes',
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      email: 'Chaim_McDermott@dana.io',
+      passed: 'No',
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      email: 'Rey.Padberg@karina.biz',
+      passed: 'No',
+    },
+    {
+      id: 11,
+      name: 'Nicholas DuBuque',
+      email: 'Rey.Padberg@rosamond.biz',
+      passed: 'Yes',
+    },
+  ];
 
   settings = {
     add: {
       addButtonContent: '<i class="nb-plus"></i>',
-      createButtonContent: '<i class="nb-checkmark"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i> ',
       cancelButtonContent: '<i class="nb-close"></i>',
+
     },
     edit: {
       editButtonContent: '<i class="nb-edit"></i>',
@@ -27,48 +97,113 @@ export class BonServiceComponent implements OnInit {
       deleteButtonContent: '<i class="nb-trash"></i>',
       confirmDelete: true,
     },
+
     columns: {
       id: {
         title: 'id',
         type: 'number',
       },
       numBS: {
-        title: 'numBS',
+        title: 'Numero BonService',
         type: 'string',
       },
       dateBS: {
-        title: 'dateBS',
+        title: 'Date BonService',
         type: 'Date',
       },
       idClient:{
         title:"Client",
+        type :  'list',
         valuePrepareFunction: (idClient) => {
             return idClient.nom;
-        }
+        },
+
+        },
+
+      email: {
+        title: 'Email',
+        filter: {
+          type: 'completer',
+          config: {
+            completer: {
+              data: this.data ,
+              searchFields: 'email',
+              titleField: 'email',
+            },
+          },
+        },
+
+
       }
-    },
+    }
   };
 
+  clients$: Observable<Client[]>;
+  private searchTerms = new Subject<string>();
 
 
-  constructor(private bonServiceService : BonServiceService) {
+  constructor(
+    private bonServiceService : BonServiceService,
+    private clienMoraletService: ClientMoraleService,
+    private clientPhysiqueService: ClientPhysiqueService,
+    private clientService: ClientService,
+    public router: Router,) {
 
   }
+  // Push a search term into the observable stream.
+  search(term: string): void {
+
+    let idContent = document.getElementById('dropdown-content');
+    idContent.style.display = "block";
+
+    this.searchTerms.next(term);
+    // let tableselect = document.getElementsByClassName('tableselect');
+    // tableselect[0].style.display = block;
+  }
+
 
   ngOnInit(): void {
     this.getAllBonServices();
+    this.getListClientsPhysiques();
+    this.getListClientMorales();
+
+    this.clients$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.clientService.searchC(term)),
+    );
   }
 
   getAllBonServices(){
     this.bonServiceService.getBonServices().subscribe(data=> this.dataSource = data)
   }
 
-  delete(bonService: BonService): void {
-    this.dataSource = this.dataSource.filter(i => i !== bonService);
-
-    this.bonServiceService.deleteBonService(bonService).subscribe();
-
+  public getListClientsPhysiques(){
+    return this.clientPhysiqueService.getClientPhysiques().subscribe(clientPhysique => this.clientPhysiques = clientPhysique);
   }
+
+  public getListClientMorales(){
+    return this.clienMoraletService.getClientMorales().subscribe(clientMorale => this.clientmorales = clientMorale)
+  }
+
+  addBonService(){
+    if (this.clientphysique.id !== undefined && this.clientMorale.id === undefined) {
+      this.client = this.clientphysique;
+    }
+    else if (this.clientphysique.id === undefined && this.clientMorale.id !== undefined) {
+      this.client = this.clientMorale;
+    }
+    else if (this.clientMorale.id !== undefined && this.clientphysique.id !== undefined) {
+      this.client = this.clientMorale;
+    }
+  }
+
+
 
 }
 
